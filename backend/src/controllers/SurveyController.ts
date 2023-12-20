3e67251ea69328f557ccecdddfcfd22c82a45f1b
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-12-20 02:20:21 -0300

    ♻️ Refatoração em: SurveyController, no Back-End da Aplicação
    No diretório: "./backend/src/repositories", implementado o segundo Repository: "SurveysRepository", visando remover do Controller: "SurveyController" a responsabilidade para com o Banco de Dados, implementando então este novo repositório, que nada mais é uma Classe: "SurveysRepository" que tem como Decorator: "EntityRepository" da Classe: "Survey" que relaciona com a respectiva Entidade do Banco de Dados, e através da Herança (do paradigma de Orientação a Objetos) estende de: "Repository" de dentro do "TypeORM" e referenciando a mesma Classe envolvida, funcionará como um repositório independente;
    Quanto ao Controller em: "./backend/src/controllers/SurveyController", a refatoração se ateve apenas à remoção dos Imports que foram realocados no repositório acima, e a alteração de tal utilizado na Constante: "surveysRepository", no Back-End da Aplicação.

--- backend/src/controllers/SurveyController.ts
+++ backend/src/controllers/SurveyController.ts
@@ -1,12 +1,12 @@
-import { getRepository } from "typeorm";
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
 
-import { Survey } from "../models/Survey";
+import { SurveysRepository } from "../repositories/SurveysRepository";
 
     class SurveyController {
         async create(request: Request, response: Response) {
             const { title, description } = request.body;
-            const surveysRepository = getRepository(Survey);
+            const surveysRepository = getCustomRepository(SurveysRepository);
             const survey = surveysRepository.create({ title, description });
 
                 await surveysRepository.save(survey);
