0397444f85253e8c8f970f24c8babc1e07159e56
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-12-20 12:45:45 -0300

    ♻️ Refatoração em: Server, no Back-End da Aplicação
    Criado o novo arquivo para a Aplicação em: "./backend/src/app.ts" onde foi realocado praticamente todo o código-fonte que antes estava no arquivo: "./backend/src/server.ts", ficando neste apenas a intanciação da Aplicação. Esta refatoração é importante, de acordo com o escopo do curso abordado em aula, por causa da implementação dos Testes, no Back-End da Aplicação.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,6 @@
-import "reflect-metadata";
-import express from "express";
+import { app } from "./app";
 
-import "./database";
-import { router } from "./routes";
-
-    const app = express();
-
-        app.use(express.json());
-        app.use(router);
-        app.listen(
-            3333,
-                () => console.log("Server is running!")
-        );
+    app.listen(
+        3333,
+            () => console.log("Server is running!")
+    );
