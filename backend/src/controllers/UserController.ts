180626d7ae99fb7e1d656dae8deadcfe4d0dade3
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-12-20 01:45:29 -0300

    ♻️ Refatoração em: UserController, no Back-End da Aplicação
    No novo diretório criado: "./backend/src/repositories", implementado o primeiro Repository: "UsersRepository", visando remover do Controller: "UserController" a responsabilidade para com o Banco de Dados, implementando então este novo repositório, que nada mais é uma Classe: "UsersRepository" que tem como Decorator: "EntityRepository" da Classe: "User" que relaciona com a respectiva Entidade do Banco de Dados, e através da Herança (do paradigma de Orientação a Objetos) estende de: "Repository" de dentro do "TypeORM" e referenciando a mesma Classe envolvida, funcionará como um repositório independente;
    Quanto ao Controller em: "./backend/src/controllers/UserController", a refatoração se ateve apenas à remoção dos Imports que foram realocados no repositório acima, e a alteração de tal utilizado na Constante: "usersRepository", no Back-End da Aplicação.

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,12 +1,12 @@
-import { getRepository } from "typeorm";
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
 
-import { User } from "../models/User";
+import { UsersRepository } from "../repositories/UsersRepository";
 
     class UserController {
         async create(request: Request, response: Response) {
             const { name, email } = request.body;
-            const usersRepository = getRepository(User);
+            const usersRepository = getCustomRepository(UsersRepository);
             const userAlreadyExists = await usersRepository.findOne({ email });
 
                 if(userAlreadyExists) {
