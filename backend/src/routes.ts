4119c8550a6a0b3a02ae2e38c98d68808cba2713
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-12-26 14:26:43 -0300

    🚩 Novo controller: NpsController, no Back-End da Aplicação
    Implementado o quinto controller em: "./backend/src/controllers/NpsController.ts" onde abstraiu toda a esquemática de  cálculo do Net Promoter Score (NPS) em relação às pesquisas enviadas e avaliadas que se encontram no Banco de Dados;
    No arquivo de rotas em: "./backend/src/routes.ts", já implementada a declaração de Classe para este novo controller criado para sua respectiva Rota e Método relacionados, no Back-End da Aplicação.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
+import { NpsController } from "./controllers/NpsController";
 import { UserController } from "./controllers/UserController";
 import { SurveyController } from "./controllers/SurveyController";
 import { AnswerController } from "./controllers/AnswerController";
 import { SendMailController } from "./controllers/SendMailController";
 
+    const npsController = new NpsController();
     const userController = new UserController();
     const surveyController = new SurveyController();
     const answerController = new AnswerController();
@@ -14,6 +16,7 @@ import { SendMailController } from "./controllers/SendMailController";
         router.post("/users", userController.create);
         router.get("/surveys", surveyController.index);
         router.post("/surveys", surveyController.create);
+        router.get("/nps/:survey_id", npsController.execute);
         router.post("/sendMail", sendMailController.execute);
         router.get("/answers/:value", answerController.execute);
 
