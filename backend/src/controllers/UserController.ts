800ece0f9040cf5b71755aa724d19805437d6c4d
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-12-26 15:00:40 -0300

    🥅 Validação com o Yup para: User, no Back-End da Aplicação
    No controller: "./backend/src/controllers/UserController", implementado o "schema" de validação para o cadastro do usuário com a utilização da dependência "Yup", seguindo o escopo abordado em aula, no Back-End da Aplicação.

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,3 +1,4 @@
+import * as yup from "yup";
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 
@@ -6,16 +7,24 @@ import { UsersRepository } from "../repositories/UsersRepository";
     class UserController {
         async create(request: Request, response: Response) {
             const { name, email } = request.body;
+            const schema = yup.object().shape({ name: yup.string().required(), email: yup.string().email().required() });
+
+                try {
+                    await schema.validate(request.body, { abortEarly: false })
+                } catch (error) {
+                    return response.status(400).json({ error: error });
+                };
+
             const usersRepository = getCustomRepository(UsersRepository);
             const userAlreadyExists = await usersRepository.findOne({ email });
 
-                if(userAlreadyExists) { return response.status(400).json({ error: "User already exists!" }); };
+                    if(userAlreadyExists) { return response.status(400).json({ error: "User already exists!" }); };
 
             const user = usersRepository.create({ name, email });
 
-                    await usersRepository.save(user);
+                        await usersRepository.save(user);
 
-                        return response.status(201).json(user);
+                            return response.status(201).json(user);
         };
     };
 
